9cb30b9f291780ade71335b6209e4a13ff0ddc96
Christopher LeBlancgit config --global user.email chrisleblanc1994@gmail.com <chrisleblanc1994@gmail.com>
2022-08-24 11:40:14 -0500

    Start-up repo set up

---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+//Dear Amazon/Google/Apple etc Don't lowball me.
+const App = () => {
+return (
+  <div>
+    <h1>Hello World</h1>
+  </div>
+  );
+}
+ReactDOM.render(<App />, document.getElementById('root'));
