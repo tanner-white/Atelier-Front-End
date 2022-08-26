e8061938ed6976ec02e53cc59d3739b4aa15e0c3
Adam Polk <adampolk91@gmail.com>
2022-08-26 15:23:50 -0500

    adds basic styling for each tile

--- src/ratingsAndReviews/components/reviewTile.jsx
+++ src/ratingsAndReviews/components/reviewTile.jsx
@@ -12,27 +12,31 @@ class ReviewTile extends React.Component {
   render() {
     return (this.state.list.results.map((review) => (
       <div className="tile" id={review.review_id}>
-        <div id="stars">
-          {`${review.rating} `}
-          Stars
-        </div>
-        <div id="user-date-stamp">
-          {review.reviewer_name}
-          {review.date}
-        </div>
-        <h4 id="summary">{review.summary}</h4>
-        <p id="body">{review.body}</p>
-        <div id="recommendation">
-          {review.recommend ? 'This person recommends this product' : ''}
-        </div>
-        <div id="responses">
-          {review.response ? `${review.response}` : ''}
-        </div>
-        <div className="pics">
-          {review.photos.map((image) => (<img id={image.id} alt="failed to load" src={image.url} />))}
-        </div>
-        <button type="submit" id="moreReviews">MORE REVIEWS</button>
-        <button type="submit">ADD REVIEW</button>
+        <span id="individual">
+          <div id="stars">
+            {`${review.rating} `}
+            Stars
+          </div>
+          <div id="user-date-stamp">
+            {review.reviewer_name}
+            {review.date}
+          </div>
+          <h4 id="summary">{review.summary}</h4>
+          <p id="body">{review.body}</p>
+          <div id="recommendation">
+            {review.recommend ? 'This person recommends this product' : ''}
+          </div>
+          <div id="responses">
+            {review.response ? `${review.response}` : ''}
+          </div>
+          <div className="pics">
+            {review.photos.map((image) => (<img id={image.id} alt="failed to load" src={image.url} />))}
+          </div>
+          <table className="buttons">
+            <button className="button" type="submit" id="moreReviews">MORE REVIEWS</button>
+            <button className="button" type="submit">ADD REVIEW</button>
+          </table>
+        </span>
       </div>
     ))
     );
