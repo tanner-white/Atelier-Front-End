a481629b7f49620410bf70ca4acf3557b22c5cbb
Tanner White <tannerjwhite618@gmail.com>
2022-08-26 11:14:24 -0500

    disable import/export on linter

--- src/overview/overviewIndex.jsx
+++ src/overview/overviewIndex.jsx
@@ -1,9 +1,10 @@
+/* eslint-disable import/extensions */
 import React from 'react';
 import ImageGallery from './components/ImageGallery.jsx';
 import BasicInfo from './components/BasicInfo.jsx';
 import StyleSelector from './components/StyleSelector.jsx';
 import AddToCart from './components/AddToCart.jsx';
-import ProductDetails from './components/ProductDetails.jsx'
+import ProductDetails from './components/ProductDetails.jsx';
 
 class Overview extends React.Component {
   constructor(props) {
