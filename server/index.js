309456bb538ce524dc4ab719344458edc8c3a41c
Jacob Gramer <grameraggie@gmail.com>
2022-08-26 10:24:01 -0500

    Implement a QuestionsList skeleton component and render to src/index.jsx

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+
 const app = express();
 app.listen(3001);
 console.log('Listening on port 3001');
--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Questions from './q&a/QuestionsApp.jsx';
 
 function App() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <Questions />
     </div>
   );
 }
