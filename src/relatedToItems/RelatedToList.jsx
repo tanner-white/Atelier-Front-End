467e3e8d1f4a99c35f8be13195e9ab7f95a78b2c
Christopher LeBlancgit config --global user.email chrisleblanc1994@gmail.com <chrisleblanc1994@gmail.com>
2022-08-29 14:11:24 -0500

    add a useEffect body without functionality.

--- src/relatedToItems/RelatedToList.jsx
+++ src/relatedToItems/RelatedToList.jsx
@@ -8,6 +8,10 @@ function RelatedToList() {
   const [relatedToCards, setRelatedToCards] = useState([0, 1, 2, 3, 4, 5, 6, 7])/* this will eventually becomes the product info of all the same category of the default product */
   const [displayedCards, setDisplayedCards] = useState([0, 1, 2]);
 
+  useEffect(() => {
+    axios.get('')
+  }, []);
+
   const goToNextSlide = () => {
     const newDisplayedCards = displayedCards.map(card => {
       return card + 1
