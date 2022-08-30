76138f2d38e0f896c380a9b2fef13be1ef43fe4d
Tanner White <tannerjwhite618@gmail.com>
2022-08-30 09:54:21 -0500

    add main arrows to carousel -- no styling

--- src/overview/components/ImageGallery.jsx
+++ src/overview/components/ImageGallery.jsx
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 function ImageGallery({ current }) {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const { length } = current;
+  function next() {
+    if (current !== length - 1) {
+      setCurrentIndex(currentIndex + 1);
+    }
+  }
+  function previous() {
+    if (current !== 0) {
+      setCurrentIndex(currentIndex - 1);
+    }
+  }
   return (
     <div className="image-gallery">
-      <img className="main-image" src={current[0]} alt="pants" />
+      <div className="carousel-left-arrow">&#8249;</div>
+      <img className="main-image" src={current[currentIndex]} alt="pants" />
+      <div className="carousel-right-arrow">&#8250;</div>
     </div>
   );
 }
