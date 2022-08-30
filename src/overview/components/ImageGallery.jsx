1403d712a3b56dd580ef77a102d8b0562ef72bac
Tanner White <tannerjwhite618@gmail.com>
2022-08-30 13:17:41 -0500

    add functionality to main arrows in image carousel

--- src/overview/components/ImageGallery.jsx
+++ src/overview/components/ImageGallery.jsx
@@ -5,20 +5,22 @@ function ImageGallery({ current }) {
   const [currentIndex, setCurrentIndex] = useState(0);
   const { length } = current;
   function next() {
-    if (current !== length - 1) {
+    if (currentIndex !== length - 1) {
       setCurrentIndex(currentIndex + 1);
+      console.log(currentIndex);
     }
   }
   function previous() {
-    if (current !== 0) {
+    if (currentIndex !== 0) {
       setCurrentIndex(currentIndex - 1);
+      console.log(currentIndex);
     }
   }
   return (
     <div className="image-gallery">
-      <div className="carousel-left-arrow">&#8249;</div>
+      <div className="carousel-left-arrow" onClick={previous}>&#8249;</div>
       <img className="main-image" src={current[currentIndex]} alt="pants" />
-      <div className="carousel-right-arrow">&#8250;</div>
+      <div className="carousel-right-arrow" onClick={next}>&#8250;</div>
     </div>
   );
 }
