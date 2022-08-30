a47e7bb5afdcf2a79fa197afdc8a8c91e3b6dd8d
Jacob Gramer <grameraggie@gmail.com>
2022-08-30 14:04:37 -0500

    Add useEffect to QuestionListEntry.jsx to make an api call for answers to each question

--- src/q&a/components/QuestionListEntry.jsx
+++ src/q&a/components/QuestionListEntry.jsx
@@ -1,9 +1,18 @@
 /* eslint-disable import/extensions */
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import axios from 'axios';
 import Answers from './Answers.jsx';
 
 function QuestionListEntry({ item }) {
+  const [answers, setAnswers] = useState([]);
+
+  useEffect(() => {
+    axios.get(`http://localhost:3001/qa/questions/${item.question_id}/answers`)
+      .then((response) => setAnswers(response.data))
+      .catch((err) => console.error(err));
+  }, [item]);
+
   return (
     <div className="question-entry">
       <div>
@@ -13,7 +22,7 @@ function QuestionListEntry({ item }) {
         <button type="button" className="link-button" onClick={() => (console.log('add and answer'))}>add answer</button>
       </div>
       <div>
-        {Object.keys(item.answers).map((answer) => <Answers answer={item.answers[answer]} />)}
+        {answers.map((answer) => <Answers answer={answer} />)}
       </div>
     </div>
   );
