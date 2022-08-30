a70dfa4c8636a27c4e36dc7e28c1dba404e81450
Tanner White <tannerjwhite618@gmail.com>
2022-08-30 15:04:11 -0500

    change qty and size buttons to dropdowns with dummy data

--- src/overview/components/AddToCart.jsx
+++ src/overview/components/AddToCart.jsx
@@ -4,14 +4,22 @@ function AddToCart() {
   const handleClick = (e) => {
     e.preventDefault();
   };
+  const qtys = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
   return (
-    <div>
-      <button type="button">Select size</button>
-      <button type="button">Qty</button>
+    <form>
+      <select value="Select size" placeholder="Select size">
+        <option>S</option>
+        <option>M</option>
+        <option>L</option>
+        <option>XL</option>
+      </select>
+      <select value="quantity">
+        {qtys.map((number) => <option>{number}</option>)}
+      </select>
       <br />
       <button type="button" onClick={handleClick}>Add to bag</button>
       <button type="button">☆</button>
-    </div>
+    </form>
   );
 }
 
