82558d592ff098f9211e0fba1809bcc238c6fe43
Adam Polk <adampolk91@gmail.com>
2022-08-30 14:13:38 -0500

    revises filter component to be stateless

--- src/ratingsAndReviews/components/filter.jsx
+++ src/ratingsAndReviews/components/filter.jsx
@@ -1,38 +1,29 @@
 import React from 'react';
 
-class Filter extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      productReviews: null,
-    };
-  }
-
-  handleSelect(event) {
+function Filter({ sortRel, sortHelp, sortNew }) {
+  const handleSelect = function(event) {
     const trigger = event.target.value;
     if (trigger === '0') {
-      this.props.sortRel();
+      sortRel();
     }
     if (trigger === '1') {
-      this.props.sortHelp();
+      sortHelp();
     }
     if (trigger === '2') {
-      this.props.sortNew();
+      sortNew();
     }
-  }
+  };
 
-  render() {
-    return (
-      <h3 className="rar_sortDropDown">
-        # reviews, sorted by:
-        <select id="reviewSelector" onChange={this.handleSelect.bind(this)}>
-          <option value="0">Relevant</option>
-          <option value="1">Helpful</option>
-          <option value="2">Newest</option>
-        </select>
-      </h3>
-    );
-  }
+  return (
+    <h3 className="rar_sortDropDown">
+      # reviews, sorted by:
+      <select id="reviewSelector" onChange={handleSelect.bind(this)}>
+        <option value="0">Relevant</option>
+        <option value="1">Helpful</option>
+        <option value="2">Newest</option>
+      </select>
+    </h3>
+  );
 }
 
 export default Filter;
